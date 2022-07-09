4bc1b5835d0c58321df91b04a191752c5a0e2ede
Jenny Zonka <jennycade@gmail.com>
2022-07-09 10:29:04 -0700

    Don't skip the test

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-xtest('renders Animle header', async () => {
+test('renders Animle header', async () => {
   render(<App />);
   const header = await screen.findByText(/Animle/i);
   expect(header).toBeInTheDocument();
